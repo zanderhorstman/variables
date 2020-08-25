aebf6b05bb064c131d1dc0282f174cf68b04553d
zanderhorstman <zanderhorstman@gmail.com>
2020-08-25 15:53:18 -0400

    1.00

---
+++ src/varTry.java
@@ -0,0 +1,13 @@
+public class varTry {
+    public static void main(String[] args) {
+        int test = 50;
+        double price = 24.99;
+        String userName = "Bobby Johnson";
+        System.out.println("Test Score: " + test +"%");
+        System.out.println("Price: $" + price);
+        System.out.println("Username: " + userName);
+        
+
+
+    }
+}
